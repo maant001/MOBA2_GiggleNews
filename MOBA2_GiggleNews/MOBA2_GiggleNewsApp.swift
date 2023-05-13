a2e41c8fd6c285a4bbc9c1466e22e1529cc71135
maant001 <mamaril_a@me.com>
2023-05-13 18:07:31 +0200

    Initial Commit

---
+++ MOBA2_GiggleNews/MOBA2_GiggleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOBA2_GiggleNewsApp.swift
+//  MOBA2_GiggleNews
+//
+//  Created by Tony Mamaril on 13.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MOBA2_GiggleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
