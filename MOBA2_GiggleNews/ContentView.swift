e7deffa05b423612752cd3b06ab59a115a19ea95
maant001 <mamaril_a@me.com>
2023-05-14 14:35:02 +0200

    - small styling changes

--- MOBA2_GiggleNews/ContentView.swift
+++ MOBA2_GiggleNews/ContentView.swift
@@ -7,84 +7,102 @@
 
 import SwiftUI
 
+
+// views
+
 struct ContentView: View {
     @State private var jokes: [Joke] = []
-       @State private var selectedJoke: Joke?
+    @State private var selectedJoke: Joke?
        
-       var body: some View {
+    var body: some View {
            
-           VStack {
+        VStack {
                
-               Text(String("Welcome to GIGGLE NEWS :) LMAO!")).bold()
+            Text(String("Welcome to GIGGLE NEWS :) LMAO!")).bold()
 
-               NavigationView {
+            NavigationView {
                    
-                   List(jokes) { joke in
+                List(jokes) { joke in
                        
-                       NavigationLink(destination: DetailView(joke: joke), tag: joke, selection: $selectedJoke) {
+                    NavigationLink(destination: DetailView(joke: joke), tag: joke, selection: $selectedJoke) {
                            
-                           Text(joke.setup)
-                       }
-                   }
-                   .onAppear(perform: loadJokes)
-                   //.navigationTitle("Giggle News")
-               }
-           }
+                        Text(joke.setup)
+                    }
+                }
+                .onAppear(perform: loadJokes)
+                //.navigationTitle("Giggle News")
+            }
+        }
            
 
-       }
+    }
        
-       private func loadJokes() {
-           let url = URL(string: "https://official-joke-api.appspot.com/random_ten")!
+    private func loadJokes() {
+        // commented out so programmer jokes appear
+        //let url = URL(string:"https://official-joke-api.appspot.com/random_ten")!
+        let url = URL(string: "https://official-joke-api.appspot.com/jokes/programming/ten")!
            
-           let task = URLSession.shared.dataTask(with: url) { data, response, error in
-               guard let data = data else {
-                   print("Error: \(error?.localizedDescription ?? "Unknown error")")
-                   return
-               }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
                
-               do {
-                   self.jokes = try JSONDecoder().decode([Joke].self, from: data)
-               } catch {
-                   print("fatal error: \(error.localizedDescription)")
-               }
-           }
+            guard let data = data else {
+                print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+               
+            do {
+                
+                self.jokes = try JSONDecoder().decode([Joke].self, from: data)
+                
+            } catch {
+                
+                print("fatal error: \(error.localizedDescription)")
+                
+            }
+        }
            
-           task.resume()
-       }
-   }
+        task.resume()
+    }
+}
 
-   struct DetailView: View {
-       let joke: Joke
+struct DetailView: View {
+    let joke: Joke
        
-       var body: some View {
+    var body: some View {
            
-           VStack(alignment: .center) {
+        VStack(alignment: .center) {
                
-               Text(joke.setup)
-                   .font(.headline)
-                   .padding()
+            Text(joke.setup)
+                .font(.headline)
+                .padding()
                
                
-               Text(joke.punchline)
-                   //.font(.subheadline)
-                   .foregroundColor(.red)
-               
-           }
-           .padding()
-           //.navigationTitle("Joke")
-       }
-   }
+            Text(joke.punchline)
+                //.font(.subheadline)
+                .font(.system(size: 23))
+                .foregroundColor(.red)
 
-   struct Joke: Codable, Identifiable, Hashable {
-       let id: Int
-       let type: String
-       let setup: String
-       let punchline: String
-   }
+               
+        }
+        .padding()
+        //.navigationTitle("Joke")
+    }
+}
 
 struct Previews_MOBA2_GiggleNewsApp_Previews: PreviewProvider {
     static var previews: some View {
        ContentView()
     }
 }
+
+
+
+// structs
+
+struct Joke: Codable, Identifiable, Hashable {
+    let id: Int
+    let type: String
+    let setup: String
+    let punchline: String
+}
+
+
