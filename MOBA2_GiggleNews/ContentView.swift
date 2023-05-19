af6f2bbc46abc1c05dec87eea281b4ac980624fb
maant001 <mamaril_a@me.com>
2023-05-19 12:49:37 +0200

    added first view to be as requested

--- MOBA2_GiggleNews/ContentView.swift
+++ MOBA2_GiggleNews/ContentView.swift
@@ -14,54 +14,71 @@ struct ContentView: View {
     @State private var jokes: [Joke] = []
     @State private var selectedJoke: Joke?
     @State private var selection = "general"
-    let data = ["general","knock-knock","programming"]
-    let data2 = ["anime","food","dad"]
-    
+    @State private var types: [String] = []
+
+
     var body: some View {
-        VStack {
-            Text("Welcome to GIGGLE NEWS :) LMAO!").bold()
-            
-            // Display buttons for each type of joke
-            HStack {
-                ForEach(data, id: \.self) { type in
-                    Button(type) {
-                        selection = type
-                        loadJokes(withParam: type)
-                    }
-                    .padding()
-                }
-            }
-            
-            HStack {
-                ForEach(data2, id: \.self) { type in
-                    Button(type) {
-                        selection = type
-                        loadJokes(withParam: type)
-                    }
-                    .padding()
-                }
-            }
-            
-            Spacer()
-            
-            NavigationView {
-                List(jokes) { joke in
-                    NavigationLink(destination: DetailView(joke: joke), tag: joke, selection: $selectedJoke) {
-                        Text(joke.setup)
+        NavigationView {
+            VStack {
+                Spacer()
+                Text("Welcome to GIGGLE NEWS :) LMAO!").bold()
+                
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                            
+                        ForEach(types, id: \.self) { type in                                        NavigationLink(destination: JokesView(type: type),
+                            label: {
+                                Text(type)
+                            })
+                            .padding()
                     }
                 }
-                .onAppear(perform: {
-                    loadJokes(withParam: selection)
-                })
+                Spacer()
             }
+            .navigationBarTitle("GIGGLE NEWS", displayMode: .inline)
         }
+        .onAppear(perform: {
+            loadCategories()
+        })
     }
     
+    func loadCategories() {
+            let urlString = "https://joke.deno.dev/type"
+            guard let url = URL(string: urlString) else { return }
+            let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+                guard let data = data else { return }
+                do {
+                    let categories = try JSONDecoder().decode([String].self, from: data)
+                    DispatchQueue.main.async {
+                        self.types = categories
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+            task.resume()
+        }
+    }
+
+struct JokesView: View {
+    let type: String
+    @State private var jokes: [Joke] = []
+    @State private var selectedJoke: Joke?
+
+    var body: some View {
+        List(jokes) { joke in
+            NavigationLink(destination: DetailView(joke: joke), tag: joke, selection: $selectedJoke) {
+                Text(joke.setup)
+            }
+        }
+        .onAppear(perform: {
+            loadJokes(withParam: type)
+        })
+        .navigationBarTitle(type.capitalized)
+    }
+
     func loadJokes(withParam param: String) {
         let urlString = "https://joke.deno.dev/type/\(param)"
-        
         print(urlString)
-        
         guard let url = URL(string: urlString) else { return }
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let data = data else { return }
@@ -78,28 +95,21 @@ struct ContentView: View {
     }
 }
 
-
 struct DetailView: View {
     let joke: Joke
        
     var body: some View {
-           
         VStack(alignment: .center) {
-               
             Text(joke.setup)
                 .font(.headline)
                 .padding()
                
-               
             Text(joke.punchline)
-                //.font(.subheadline)
                 .font(.system(size: 23))
                 .foregroundColor(.red)
-
-               
         }
         .padding()
-        //.navigationTitle("Joke")
+        .navigationTitle("Joke")
     }
 }
 
@@ -108,7 +118,6 @@ struct Previews_MOBA2_GiggleNewsApp_Previews: PreviewProvider {
        ContentView()
     }
 }
-
 
 
 // structs
